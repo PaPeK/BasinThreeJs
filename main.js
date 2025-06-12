43756be4be8966bcd498795148840781526e60d7
ISIN KOSEMEN <isinkosemen@gmail.com>
2025-06-12 14:24:08 +0200

    locally install modules and install vite

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+// public/main.js
+import * as THREE        from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GUI }           from 'lil-gui';
 
 function main() {
 
